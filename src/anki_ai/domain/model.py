2512b7f1ad9c9c19763442d89ebbf33fdf099f21
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-08-18 20:10:22 -0400

    feat(domain): create Deck and Note classes, porting what we have already created in a notebook

---
+++ src/anki_ai/domain/model.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+from typing import List, Optional
+
+
+class Note:
+    def __init__(self, front: str, back: str, tags: Optional[List[str]]) -> None:
+        self.front = front
+        self.back = back
+        self.tags = tags
+
+    def __repr__(self) -> str:
+        return f"Note(front={self.front}, back={self.back}, tags={self.tags}"
+
+
+class Deck:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self._collection: List = []
+
+    def from_txt(self, fpath: Path, ignore_media: bool = True) -> None:
+        with open(fpath) as f:
+            for i, line in enumerate(f):
+                if ignore_media:
+                    if "<img src=" in line:
+                        continue
+
+                try:
+                    front, back, _, _, _, tags = line.split("\t")
+
+                    # convert tags as a list of str
+                    tags = tags.replace("\n", "")
+                    tags = list(tags.split(" "))
+
+                    # print(f"FRONT #{i}: {tags} {front}\nBACK  #{i}: {back}\n")
+                    self._collection.append(Note(front=front, back=back, tags=tags))
+                except ValueError:
+                    print(f"Was not able to process line {i}: {line}")
