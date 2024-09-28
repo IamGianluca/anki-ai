6e8c357587f71715c9aa79794795db28ce8a4552
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2024-09-28 18:32:01 -0400

    refactor(entrypoints): clean up code

--- src/anki_ai/entrypoints/api_server.py
+++ src/anki_ai/entrypoints/api_server.py
@@ -8,13 +8,11 @@ from vllm.entrypoints.logger import RequestLogger
 from vllm.entrypoints.openai.protocol import ChatCompletionRequest
 from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
 
-MODEL_DIR = ["meta-llama/Meta-Llama-3.1-8B-Instruct"]
-
 
 def serving_chat():
     model = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     max_model_len = 4_096
-    # chat_template = "./template_llama31.jinja"
+    chat_template = "./template_llama31.jinja"
     engine_args = AsyncEngineArgs(model=model, max_model_len=max_model_len)
     engine = AsyncLLMEngine.from_engine_args(engine_args)
 
@@ -22,12 +20,12 @@ def serving_chat():
     serving_chat = OpenAIServingChat(
         engine,
         model_config=model_config,
-        chat_template=None,
-        served_model_names=MODEL_DIR,
+        chat_template=chat_template,
+        served_model_names=[model],
         response_role="assistant",
         lora_modules=[],
         prompt_adapters=[],
-        request_logger=RequestLogger(max_log_len=2048),
+        request_logger=RequestLogger(max_log_len=2_048),
     )
 
     return serving_chat
@@ -42,17 +40,6 @@ def run_server():
         return await chat.create_chat_completion(request)
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-# def start_live_server():
-#     proc = multiprocessing.Process(target=run_server, args=(serving_chat,))
-#     proc.start()
-#     time.sleep(2)  # Give the server some time to start
-#
-#     yield "http://localhost:8000"
-#
-#     proc.terminate()
-#     proc.join()
 
 
 if __name__ == "__main__":
--- tests/integration/test_chat_completion.py
+++ tests/integration/test_chat_completion.py
@@ -3,9 +3,16 @@ from openai.types.chat.chat_completion import ChatCompletion
 
 from anki_ai.adapters.chat_completion import get_chat_completion
 
+# We are running this test both against a real vLLM API server (nullable=False)
+# and with the nullable. The goal is to ensure our stub behaves like the
+# original third-party dependency.
+
 
 @pytest.mark.parametrize("nullable", [True, False])
 def test_chat_completion(nullable):
+    # TODO(grossi): Create fixture to spin us real vLLM API server for real
+    # integration test.
+
     # given
     chat = get_chat_completion(nullable=nullable)
     model = "meta-llama/Meta-Llama-3.1-8B-Instruct"
